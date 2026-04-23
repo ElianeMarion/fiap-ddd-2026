d46d814f279bf6eb41572022b6790f80c18e3c88
Eliane <profeliane.rosa@fiap.com.br>
2026-04-23 09:40:49 -0300

    Material de apoio de herança e exercícios

--- ddd-heranca/src/br/com/fiap/heranca/models/Vendedor.java
+++ ddd-heranca/src/br/com/fiap/heranca/models/Vendedor.java
@@ -3,7 +3,7 @@ package br.com.fiap.heranca.models;
 public class Vendedor extends Funcionario{
     private double comissao;
     public double calculoComissao(double valorVendido){
-        return  valorVendido + 1.1 + getSalario();
+        return  valorVendido * 1.1 + getSalario();
     }
     public double calculaBonificacao(){
         return super.calculaBonificacao() + 500;
