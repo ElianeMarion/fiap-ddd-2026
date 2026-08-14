92423975c2d6ec5cfe96b83324792662f540ef5c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-14 09:55:12 -0300

    Acesso ao banco de dados

---
+++ ddd-agenda/src/br/com/fiap/agenda/dao/ConnectionFactory.java
@@ -0,0 +1,19 @@
+package br.com.fiap.agenda.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection obterConexao(){
+        Connection conexao = null;
+        try{
+            conexao = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl",
+                    "pf1551", "fiap26");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conexao;
+    }
+}
